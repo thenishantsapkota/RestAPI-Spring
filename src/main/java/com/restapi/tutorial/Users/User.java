c8f11d439e8b3b3b9cc2025285e173d5f68392d0
Nishant Sapkota <snishant306@gmail.com>
2022-07-13 23:38:31 +0545

    refactor: made username an unique field

--- src/main/java/com/restapi/tutorial/Users/User.java
+++ src/main/java/com/restapi/tutorial/Users/User.java
@@ -1,5 +1,6 @@
 package com.restapi.tutorial.Users;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class User {
     private long id;
     private String name;
     private Boolean isAdmin;
+    @Column(unique = true)
     private String username;
     private String password;
 
